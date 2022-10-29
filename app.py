3502199d21f2752107b2a44c0ceea903c0bb6cc8
jdreed129 <jreed2@iwu.edu>
2022-10-29 09:07:42 -0500

    Fixed schema; added initdb config

--- app.py
+++ app.py
@@ -42,12 +42,11 @@ def init_db():
     db.commit()
 
 
-@app.cli.command('initdb') #decorator: on command line you can use initdb: lets you run your code from the command line, you can initalize data base without running the program
+@app.cli.command('initdb')
 def initdb_command():
     """Creates the database tables."""
     init_db()
     print('Initialized the database.')
-
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -57,14 +56,11 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
-
 
 
 @app.route('/', methods=['GET']) #specifing in the url what resource you want to access. Backend takes request and figures out what resource this is for.
@@ -79,49 +75,3 @@ def show_entries():
         cur = db.execute("SELECT title,text,category,id from entries WHERE category = ? ORDER BY id desc", [request.args["category"]])
         entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries, distinct_cats=distinct_cats) #puts the list into a template
-
-@app.route('/add', methods=['POST']) #url with /add will trigger the add fucntion, if the POST method is used
-def add_entry():
-    db = get_db() #looks into the database
-    db.execute('insert into entries (title, category, text) values (?, ?, ?)', #SQL injection
-               [request.form['title'], request.form['category'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-# Routes you to /delete, it is expecting to recieve a POST Command from show_entries.html
-# No arguments are needed, and it will redirect you to the show_entries function on app.py where it will
-# have the same webpage as before, but with one certain post deleted (depending on which entry's
-# delete button (from the show_entries.html) was pressed)
-@app.route('/delete', methods=['POST']) #url with /delete will trigger the delete fucntion, if the POST method is used
-def delete_entry():
-   db = get_db() #looks into the database
-   db.execute("DELETE from entries WHERE id = ?", [request.form['item_id']])
-   db.commit()
-   flash('Entry Successfully Deleted')
-   return redirect(url_for('show_entries'))
-
-# Routes you to /edit, it is expecting to recieve a POST Command from edit_post.html
-# No arguments are needed, and it will redirect you to the show_entries function on app.py where it will
-# have the same webpage as before, but with one certain post edited (depending on which entry's
-# edit button (from the show_entries.html) was pressed)
-@app.route('/edit', methods=['POST'])
-def edit_entry():
-    db = get_db()
-    db.execute('UPDATE entries SET title = ?, category = ?, text = ? WHERE id = ?',
-               [request.form['title'], request.form['category'], request.form['text'], request.form['item_id']])
-    db.commit()
-    flash('Entry Successfully Updated')
-    return redirect(url_for('show_entries'))
-
-#Routes you to /edit_button, it is expecting to recieve a POST Command from show_entries.html.
-# No arguments are needed, and it will take the item_id (from the selected post) and render the edit post form in
-# edit_post.html. Through the item_id it will then prepopulate the form inputs with the current information (i.e. current
-# titles, categories, text, and ids).
-@app.route('/edit_button', methods=['POST'])
-def edit_button():
-    db = get_db()
-    cur = db.execute('select title,category,text,id from entries WHERE id = ? order by id desc ', [request.form['item_id']])  # gets entries from the data base, cur pointer into the database
-    entries = cur.fetchone()
-    return render_template('edit_post.html', entries=entries)
-
--- schema.sql
+++ schema.sql
@@ -1,14 +1,18 @@
+drop table if exists characters;
+drop table if exists relationships;
+
 CREATE TABLE characters (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT not null,
     description TEXT
-)
+);
 
 CREATE TABLE relationships (
-    character1 PRIMARY KEY AUTOINCREMENT,
-    character2 PRIMARY KEY AUTOINCREMENT,
-    type = TEXT not null,
+    character1 INT,
+    character2 INT,
+    type TEXT not null,
     description TEXT,
-    FOREIGN KEY character1 references characters,
-    FOREIGN KEY character2 references characters
-)
+    PRIMARY KEY (character1, character2),
+    FOREIGN KEY (character1) references characters(id),
+    FOREIGN KEY (character2) references characters(id)
+);
