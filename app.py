2ae823f116b786e7913dd81b831e3c24bf57f988
Jeff <jchai@iwu.edu>
2022-11-08 11:10:01 -0600

    fixed delete/edit functions

--- app.py
+++ app.py
@@ -111,25 +111,23 @@ def add_relationship():
 @app.route('/delete', methods=['POST'])
 def delete_character():
     db = get_db()
-    db.execute('delete from characters where name = ?', [request.form['name']])
+    db.execute('delete from characters where id = ?', [request.form['id']])
     db.commit()
     flash('character was deleted')
     return redirect(url_for('show_tree'))
 
-
 @app.route('/edit', methods=['POST'])
 def edit_character():
     db = get_db()
-    cur = db.execute('select name from characters where name = ?', [request.form['name']])
+    cur = db.execute('select name, id from characters where id = ?', [request.form['id']])
     characters = cur.fetchone()
     flash('moved to edit page')
     return render_template('edit.html', characters=characters)
 
-
 @app.route('/save_edit', methods=['POST'])
 def save_edit_character():
     db = get_db()
-    db.execute('update characters set name = ? where name = ?', [request.form['name'], request.form['rename']])
+    db.execute('update characters set name = ? where id = ?', [request.form['name'], request.form['id']])
     db.commit()
     flash('character was edited')
     return redirect(url_for('show_tree'))
