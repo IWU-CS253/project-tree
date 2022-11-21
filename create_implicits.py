ea59add85a52a7c9d6f0965d2eb2b28c44697070
jdreed129 <jreed2@iwu.edu>
2022-11-21 11:38:44 -0600

    Fixed bugs in create_implicit; add merge_implicits to incorporate generated implicits into relationship list passed to html

--- create_implicits.py
+++ create_implicits.py
@@ -5,8 +5,9 @@
 # found in chapter 6 of Problem Solving with Algorithms and Data Structures using Python, by Miller and Ranum
 
 class Character:
-    def __init__(self, num):
+    def __init__(self, num, name):
         self.id = num
+        self.name = name
         self.children = []  # list of children characters
         self.parents = []  # list of parent characters
         self.niblings = []  # list of nibling characters
@@ -28,18 +29,16 @@ class Character:
 class Graph:
     def __init__(self):
         self.charList = {}
-        self.numCharacter = 0
         self.charKey = {}
 
-    def add_character(self, key):  # adds a character to the graph
-        self.numCharacter += 1
-        newCharacter = Character(key)
-        self.charList[key] = newCharacter
-        return newCharacter
+    def add_character(self, key, name):  # adds a character to the graph
+        new_character = Character(key, name)
+        self.charList[key] = new_character
+        return new_character
 
     def add_Parent_Child(self, p, c):
-        self.charList[p].addChild(self.charList[c])
-        self.charList[c].addParent(self.charList[c])
+        self.charList[p].add_child(self.charList[c])
+        self.charList[c].add_parent(self.charList[c])
 
     def get_char(self, n):  # checks whether 'n' is in the graph
         if n in self.charList:
@@ -55,14 +54,14 @@ def create_graph(characters, relationships):
     graph = Graph()
     sib_incrementer = 1  # Used to generate sibling numbers; always increases every time one is assigned
     for character in characters:
-        graph.addCharacter(character['ID'])
+        graph.add_character(character['ID'], character['NAME'])
     for relationship in relationships:
-        char1 = graph.getChar(relationship['CHARACTER1'])
-        char2 = graph.getChar(relationship['CHARACTER2'])
+        char1 = graph.get_char(relationship['CHARACTER1'])
+        char2 = graph.get_char(relationship['CHARACTER2'])
         # Adds parents and children to the graph
         if relationship['TYPE'] == 'Parent - Child':
-            char1.addChild(char2)
-            char2.addParent(char1)
+            char1.add_child(char2)
+            char2.add_parent(char1)
 
         # Adds siblings to the graph, and adds implicit siblings transitively
         # Eg if a & b are siblings and b & c are siblings, makes them all siblings via sibling_nums
@@ -95,44 +94,44 @@ def implicit_siblings(graph):
     # will not conflict with existing ones.
     sib_incrementor = 1
     for char in graph.charList:
-        if graph.getChar(char).sibling_num > sib_incrementor:
-            sib_incrementor = graph.getChar(char).sibling_num + 1
+        if graph.get_char(char).sibling_num > sib_incrementor:
+            sib_incrementor = graph.get_char(char).sibling_num + 1
 
     for char in graph.charList:
-        if len(graph.getChar(char).children) > 1:  # if a character has multiple children
+        if len(graph.get_char(char).children) > 1:  # if a character has multiple children
             # Find any existing sibling numbers among the children
             sibling_numbers = []
-            for child in graph.getChar(char).children:
+            for child in graph.get_char(char).children:
                 if child.sibling_num != 0:
                     sibling_numbers.append(child.sibling_num)
 
             if len(sibling_numbers) == 0:  # If none had a sibling number, generate a new one and assign it to all
-                for child in graph.getChar(char).children:
+                for child in graph.get_char(char).children:
                     child.sibling_num = sib_incrementor
                     sib_incrementor += 1
 
             # If all have sibling numbers, and they are all the same, move on to the next character
-            if len(sibling_numbers) == len(graph.getChar(char).children) and len(set(sibling_numbers)) == 1:
+            if len(sibling_numbers) == len(graph.get_char(char).children) and len(set(sibling_numbers)) == 1:
                 continue
 
             if len(sibling_numbers) == 1:  # If only one had a sibling number, give it to the rest
-                for child in graph.getChar(char).children:
+                for child in graph.get_char(char).children:
                     child.sibling_num = sibling_numbers[0]
 
             # If there are multiple sibling numbers, all need to be unified, along with characters with those numbers
             for subChar in graph.charList:
-                if graph.getChar(subChar).sibling_num in sibling_numbers:
-                    graph.getChar(subChar).sibling_num = sibling_numbers[0]
+                if graph.get_char(subChar).sibling_num in sibling_numbers:
+                    graph.get_char(subChar).sibling_num = sibling_numbers[0]
 
     return graph
 
 
 def implicit_parents(graph):
     for character in graph.charList:
-        char = graph.getChar(character)
+        char = graph.get_char(character)
         if len(char.parents) > 0 and char.sibling_num != 0:  # If a character has a parent and has siblings
             for subCharacter in graph.charList:
-                subChar = graph.getChar(subCharacter)
+                subChar = graph.get_char(subCharacter)
                 # If a character is one of the siblings of the above character, give them all of their parents, and
                 # the parents the sibling
                 if subChar.sibling_num == char.sibling_num:
@@ -146,11 +145,11 @@ def implicit_parents(graph):
 
 def implicit_piblings(graph):
     for character in graph.charList:
-        char = graph.getChar(character)
+        char = graph.get_char(character)
         if len(char.children) > 0 and char.sibling_num != 0:  # If a character has children and siblings:
             for child in char.children:
                 for sibling in graph.charList:
-                    sib = graph.getChar(sibling)
+                    sib = graph.get_char(sibling)
                     if sib.sibling_num == char.sibling_num and sib != char:
                         child.piblings.append(sib)
                         sib.niblings.append(child)
@@ -159,7 +158,7 @@ def implicit_piblings(graph):
 
 def implicit_cousins(graph):
     for character in graph.charList:
-        char = graph.getChar(character)
+        char = graph.get_char(character)
         if len(char.piblings) != 0:
             for pibling in char.piblings:
                 for child in pibling.children:
@@ -176,7 +175,7 @@ def recur_grandparents(graph, base_char, cur_char, level=0):
     for p in cur_char.parents:  # Looping through all parents' parents
         if p not in base_char.grandparents:
             if level == 0:
-                cur_char.grandparents[p.id] = 'Grandparent'
+                base_char.grandparents[p.id] = 'Grandparent'
                 p.grandchildren[base_char.id] = 'Grandchild'
             else:
                 greats = ''
@@ -191,14 +190,15 @@ def recur_grandparents(graph, base_char, cur_char, level=0):
 
 def implicit_grandparents(graph):
     for character in graph.charList:
-        char = graph.getChar(character)
+        char = graph.get_char(character)
         if len(char.parents) != 0:
-            for v in char.parents:
-                graph = recur_grandparents(graph, char, v)
+            for parent in char.parents:
+                graph = recur_grandparents(graph, char, parent)
     return graph
 
 
 def add_implicits(graph):
+    """Takes a graph and applies all implicit relationship functions to it, returning the new graph."""
     graph = implicit_siblings(graph)
     graph = implicit_parents(graph)
     graph = implicit_piblings(graph)
@@ -207,9 +207,66 @@ def add_implicits(graph):
     return graph
 
 
+def merge_implicits(characters, relationships):
+    """Given a set of characters and relationships (sqlite objects including, at minimum, character ids and relationship
+    types), creates a graph object to calculate additional implicit relationships. Adds those relationships to the
+     relationship set, and returns it, along with the resulting graph."""
+
+    graph = add_implicits(create_graph(characters, relationships))
+
+    rel_list = []  # Simplifies avoiding duplicates, since relationship objects are fairly complex and difficult to
+    # check exactly without tracking descriptions across the whole file
+    for relationship in relationships:
+        rel_list.append((relationship['CHARACTER1'], relationship['CHARACTER2'], relationship['TYPE']))
+        print((relationship['CHARACTER1'], relationship['CHARACTER2'], relationship['TYPE']))
+
+    for character in graph.charList:
+        char = graph.get_char(character)
+        if char.sibling_num != 0:  # Ensure that the character has any siblings before looping through and checking for them
+            for sibling in graph.charList:
+                sib = graph.get_char(sibling)
+                if sib.sibling_num == char.sibling_num and char.id != sib.id:
+                    # The one ready to put in the relationships list
+                    full_rel = {'CHARACTER1': char.id, 'CHARACTER2': sib.id, 'TYPE': 'Sibling - Sibling',
+                                'CHAR1_NAME': char.name, 'CHAR2_NAME': sib.name}
+                    rel = (char.id, sib.id, 'Sibling - Sibling')
+                    print(rel)
+                    alt_rel = (sib.id, char.id, 'Sibling - Sibling')
+                    print(alt_rel)
+                    if rel not in rel_list and alt_rel not in rel_list:
+                        relationships.append(full_rel)
+                        rel_list.append(rel)
+        for parent in char.parents:
+            full_rel = {'CHARACTER1': parent.id, 'CHARACTER2': char.id, 'TYPE': 'Parent - Child',
+                        'CHAR1_NAME': parent.name, 'CHAR2_NAME': char.name}
+            rel = (parent.id, char.id, 'Parent - Child')
+            if rel not in rel_list:
+                relationships.append(full_rel)
+        for pibling in char.piblings:
+            relationships.append({'CHARACTER1': pibling.id, 'CHARACTER2': char.id, 'TYPE': 'Pibling - Nibling',
+                                  'CHAR1_NAME': pibling.name, 'CHAR2_NAME': char.name})
+        for cousin in char.cousins:
+            full_rel = {'CHARACTER1': char.id, 'CHARACTER2': cousin.id, 'TYPE': 'Cousin - Cousin',
+                        'CHAR1_NAME': char.name, 'CHAR2_NAME': cousin.name}
+            rel = (char.id, cousin.id, 'Cousin - Cousin')
+            alt_rel = (cousin.id, char.id, 'Cousin - Cousin')
+            if rel not in rel_list and alt_rel not in rel_list:
+                relationships.append(full_rel)
+                rel_list.append(rel)
+        for grandparent in char.grandparents:
+            gparent = graph.get_char(grandparent)  # Since iterating through gives us the keys, aka the ids
+            relationships.append({'CHARACTER1': gparent.id, 'CHARACTER2': char.id,
+                                  'TYPE': char.grandparents[grandparent] + ' - ' + gparent.grandchildren[char.id],
+                                  'CHAR1_NAME': gparent.name, 'CHAR2_NAME': char.name})
+
+    return relationships, graph
+
+
 def test_graph():
-    characters = [{'ID': 1}, {'ID': 2}, {'ID': 3}, {'ID': 4}, {'ID': 5}, {'ID': 6}, {'ID': 7}, {'ID': 8}, {'ID': 9},
-                  {'ID': 10}, {'ID': 11}, {'ID': 12}]
+    characters = [{'ID': 1, 'NAME': 'A'}, {'ID': 2, 'NAME': 'B'}, {'ID': 3, 'NAME': 'C'}, {'ID': 4, 'NAME': 'D'},
+                  {'ID': 5, 'NAME': 'E'},
+                  {'ID': 6, 'NAME': 'F'}, {'ID': 7, 'NAME': 'G'}, {'ID': 8, 'NAME': 'H'}, {'ID': 9, 'NAME': 'I'},
+                  {'ID': 10, 'NAME': 'J'}, {'ID': 11, 'NAME': 'K'}, {'ID': 12, 'NAME': 'L'}, {'ID': 13, 'NAME': 'M'}]
     # This format mirrors the way sqlite objects are structured, and how we will access the relevant attributes
     relationships = [{'CHARACTER1': 1, 'CHARACTER2': 2, 'TYPE': 'Parent - Child'},
                      {'CHARACTER1': 1, 'CHARACTER2': 3, 'TYPE': 'Parent - Child'},
@@ -219,17 +276,17 @@ def test_graph():
                      {'CHARACTER1': 3, 'CHARACTER2': 7, 'TYPE': 'Sibling - Sibling'},
                      {'CHARACTER1': 8, 'CHARACTER2': 9, 'TYPE': 'Parent - Child'},
                      {'CHARACTER1': 9, 'CHARACTER2': 1, 'TYPE': 'Sibling - Sibling'},
-                     {'CHARACTER1': 9, 'CHARACTER2': 10, 'TYPE': 'Parent - Child'},
                      {'CHARACTER1': 10, 'CHARACTER2': 11, 'TYPE': 'Parent - Child'},
-                     {'CHARACTER1': 11, 'CHARACTER2': 12, 'TYPE': 'Parent - Child'}]
+                     {'CHARACTER1': 11, 'CHARACTER2': 12, 'TYPE': 'Parent - Child'},
+                     {'CHARACTER1': 12, 'CHARACTER2': 13, 'TYPE': 'Parent - Child'}]
 
-    base_graph = createGraph(characters, relationships)
+    base_graph = create_graph(characters, relationships)
     graph = add_implicits(base_graph)
-    for child in graph.getChar(1).children:
+    for child in graph.get_char(1).children:
         print(child.id)
-    print(graph.getChar(8).children[0].id)
+    print(graph.get_char(8).children[0].id)
     for character in graph.charList:
-        char = graph.getChar(character)
+        char = graph.get_char(character)
         print('character: ', str(character), ' sibnum: ', str(char.sibling_num))
         for parent in char.parents:
             if parent:
@@ -245,4 +302,5 @@ def test_graph():
                 print('character: ', str(character), ' Cousin: ', str(cousin.id))
         for grandparent in char.grandparents:
             if grandparent:
-                print('character: ', str(character), ' Grandparent: ', str(grandparent), str(char.grandparents[grandparent]))
+                print('character: ', str(character), ' Grandparent: ', str(grandparent),
+                      str(char.grandparents[grandparent]))
