ce37c1d0654c80f8ef256706456132108ab6be24
Jeff <jchai@iwu.edu>
2022-12-01 19:17:29 -0600

    fixed username_id

--- schema.sql
+++ schema.sql
@@ -1,9 +1,22 @@
+drop table if exists accounts;
+drop table if exists trees;
 drop table if exists characters;
 drop table if exists relationships;
-drop table if exists trees;
 drop table if exists colors;
-drop table if exists ids;
-drop table if exists accounts;
+
+
+CREATE TABLE accounts   (
+    username_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE trees  (
+    tree_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree_name TEXT,
+    username_id INT,
+    FOREIGN KEY (username_id) references accounts(username_id)
+);
 
 CREATE TABLE characters (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,25 +37,9 @@ CREATE TABLE relationships (
     FOREIGN KEY (character2) references characters(id) ON DELETE CASCADE
 );
 
-CREATE TABLE trees  (
-    tree_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tree_name TEXT
-);
-
 CREATE TABLE colors  (
     tree_id_color INT,
     color TEXT,
     type TEXT,
     FOREIGN KEY (tree_id_color) references trees(tree_id) ON DELETE CASCADE
-);
-
-CREATE TABLE  ids  (
-    tree_id INT,
-    username_id INT
-);
-
-CREATE TABLE accounts   (
-    username_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password_hash TEXT
 );
